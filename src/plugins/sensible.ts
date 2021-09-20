d7c01dae87bdc88f3b4f4d3031a10c5472068081
mrLuisFer <mrluisfeer@gmail.com>
2021-09-19 22:48:48 -0500

    [feat] add ts details for plugins

--- src/plugins/sensible.ts
+++ src/plugins/sensible.ts
@@ -1,14 +1,16 @@
 import fp from 'fastify-plugin'
 import { FastifyInstance } from 'fastify'
+import { SensibleOptions } from 'fastify-sensible'
 
 /**
  * This plugins adds some utilities to handle http errors
  * @see https://github.com/fastify/fastify-sensible
  */
-const sensible = fp(async function (fastify: FastifyInstance, opts: unknown) {
+export default fp<SensibleOptions>(async function (
+  fastify: FastifyInstance,
+  opts: unknown,
+) {
   fastify.register(require('fastify-sensible'), {
     errorHandler: false,
   })
 })
-
-export default sensible
--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -3,11 +3,22 @@ import { FastifyInstance } from 'fastify'
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
+export interface SupportPluginOptions {
+  // Specify Support plugin options here
+}
 
-const support = fp(async function (fastify: FastifyInstance, opts: unknown) {
+export default fp<SupportPluginOptions>(async function (
+  fastify: FastifyInstance,
+  opts: unknown,
+) {
   fastify.decorate('someSupport', function () {
     return 'hugs'
   })
 })
 
-export default support
+// When using .decorate you have to specify added properties for Typescript
+declare module 'fastify' {
+  export interface FastifyInstance {
+    someSupport(): string
+  }
+}
