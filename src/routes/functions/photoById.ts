e1fcbe1bd56b0835d96b47026d40cab85c34328f
mrLuisFer <mrluisfeer@gmail.com>
2021-09-21 20:19:27 -0500

    [feat] add find method and handler to send photo by id

--- src/routes/functions/photoById.ts
+++ src/routes/functions/photoById.ts
@@ -5,6 +5,7 @@ import {
   FastifyRequest,
   RequestParamsDefault,
 } from 'fastify'
+import { photosArray } from '../../utils/db/photos'
 
 /**
  * @route GET /photos/{id}
@@ -19,11 +20,23 @@ const photoById: FastifyPluginAsync = async (
     '/photos/:id',
     async function (request: FastifyRequest, reply: FastifyReply) {
       console.log(request.params)
-      const params: RequestParamsDefault = request.params
+      // It should not be type Any but if it does not have it, it detects it as Unknown.
+      const params: RequestParamsDefault | any = request.params
+      const id: string = params.id.toString()
 
-      reply.send({
-        id: params?.id,
-      })
+      const photo = photosArray.find((photo) => id === photo.id)
+
+      if (photo !== undefined) {
+        const statusCode: number = 200
+        reply.status(statusCode).send({
+          statusCode,
+          data: photo,
+        })
+      } else {
+        reply.status(404).send({
+          error: 'Photo not found',
+        })
+      }
     },
   )
 }
