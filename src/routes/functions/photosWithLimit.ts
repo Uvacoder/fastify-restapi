394923b1e914c77d03faf60ccd41b4a6de5e8d62
mrLuisFer <mrluisfeer@gmail.com>
2021-09-21 21:35:36 -0500

    [feat] add details to photosLimit

--- src/routes/functions/photosWithLimit.ts
+++ src/routes/functions/photosWithLimit.ts
@@ -3,11 +3,13 @@ import {
   FastifyPluginAsync,
   FastifyReply,
   FastifyRequest,
+  RequestParamsDefault,
 } from 'fastify'
 import { AutoloadPluginOptions } from 'fastify-autoload'
+import { photosArray } from '../../utils/db/photos'
 
 /**
- * @route GET /photos?:{limit}
+ * @route GET /photos/limit/{limit}
  * @param {FastifyInstance} fastify Fastify Instance
  * @param {Record<never, never>} opts Opts
  */
@@ -16,9 +18,21 @@ const photosWithLimit: FastifyPluginAsync = async (
   opts?: Partial<AutoloadPluginOptions>,
 ): Promise<void> => {
   fastify.get(
-    '/example',
+    '/photos/limit/:limit',
     async function (request: FastifyRequest, reply: FastifyReply) {
-      return 'this is an example'
+      const params: RequestParamsDefault | any = request.params
+      const limit: number = Number(params.limit)
+      console.log(limit)
+      if (limit > 0) {
+        const photos = photosArray.slice(0, limit)
+        const statusCode: number = 200
+        reply.status(statusCode).send({ statusCode, data: photos })
+      } else {
+        reply.status(404).send({
+          statusCode: 404,
+          error: 'limit must be a number higher than 1',
+        })
+      }
     },
   )
 }
